1602a02cd869ad5978c4141eb6be06ad6dad8f81
sumit roy <sumit.roy@cerner.com>
2019-11-10 17:26:13 +0530

    Updates

---
+++ precache-manifest.3b135d4a75006f31bd5cc69c8aa665e9.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4798e5ef5733c74c9d25c0fe15692623",
+    "url": "/resume-portfolio/index.html"
+  },
+  {
+    "revision": "896c717871bcce6286c6",
+    "url": "/resume-portfolio/static/css/main.c73eccdb.chunk.css"
+  },
+  {
+    "revision": "e5923659975685242bb4",
+    "url": "/resume-portfolio/static/js/2.c90139e4.chunk.js"
+  },
+  {
+    "revision": "896c717871bcce6286c6",
+    "url": "/resume-portfolio/static/js/main.0caf4ae2.chunk.js"
+  },
+  {
+    "revision": "5a148c72301ab1bb1585",
+    "url": "/resume-portfolio/static/js/runtime-main.4bfebff9.js"
+  },
+  {
+    "revision": "229e949106b28921002a57d62b3c685d",
+    "url": "/resume-portfolio/static/media/img1.229e9491.jpg"
+  },
+  {
+    "revision": "f556a2c3f37c228f95a5eac3fae8c4a7",
+    "url": "/resume-portfolio/static/media/video.f556a2c3.webm"
+  },
+  {
+    "revision": "ffcd876c5561db066c09f5922b384473",
+    "url": "/resume-portfolio/static/media/video.ffcd876c.mp4"
+  }
+]);
